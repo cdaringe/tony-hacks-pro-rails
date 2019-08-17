93459b875a112ca37360d29a32105d2ed778cece
c0d01a5 <christopher.dieringer@walmartlabs.com>
2019-08-17 16:24:18 -0700

    feat: init

--- control.lua
+++ control.lua
@@ -1,20 +1,20 @@
-SEARCH_RANGE = 1
-function on_pre_death(evt, cause)
-  player = game.players[evt.player_index]
-  local trains = player.surface.find_entities_filtered
-  {
-    area =
-    {{player.position.x - SEARCH_RANGE, player.position.y - SEARCH_RANGE},
-    {player.position.x + SEARCH_RANGE, player.position.y + SEARCH_RANGE}},
-    type= "locomotive"
-  }
-  if #trains > 0 then
-    player.character.health = 1
-  end
+function on_player_changed_surface(evt, cause)
+  print(cause)
+  -- player = game.players[evt.player_index]
+  -- local trains = player.surface.find_entities_filtered
+  -- {
+  --   area =
+  --   {{player.position.x - SEARCH_RANGE, player.position.y - SEARCH_RANGE},
+  --   {player.position.x + SEARCH_RANGE, player.position.y + SEARCH_RANGE}},
+  --   type= "locomotive"
+  -- }
+  -- if #trains > 0 then
+  --   player.character.health = 1
+  -- end
 end
 script.on_event(defines.events.on_pre_player_died, on_pre_death)
-
-function on_player_changed_force(evt)
-  game.player.print(evt)
-end
-script.on_event(defines.events.on_player_changed_force, on_player_changed_force)
+-- onmove?, test for rail
+-- get rail entity, query for connect rails
+  -- get user direction, move player on rail, animating between entities in rail segment
+  -- at end of segment, check for next segment, continue
+  -- when grind is no longer held down, stop
---
+++ src/physics.lua
@@ -0,0 +1,10 @@
+local physics = {}
+
+function physics.get_trajectory(p1, p2)
+  -- -- print(p1, p2)
+  -- local dx = p2[1] - p1[2]
+  -- local dy =
+  -- return { dx , p2[1] - p1[2] }
+end
+
+return physics
